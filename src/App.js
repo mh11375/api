3d3dc46225974876463bbbdb39ea3dca518dd305
michelle <michellehsieh@yahoo.co.uk>
2019-05-01 20:55:03 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+
+
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      item: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      console.log("this is line 20 in app.js", data);
+      this.setState({
+        isLoaded: true,
+        items: data,
+      });
+    });
+  }
+
+
+  render() {
+    var { isLoaded, items } = this.state;
+    if(!isLoaded){
+      return <div>Loading...</div>;
+    }else {
+    return(
+      <div className="App">
+      <div className="Names">
+      <ul>
+      {items.map(el => {
+        return (
+          <li key={el.id}>
+          Name: {el.name} | UserName: {el.username} | {' '}
+          <a href={`https://${el.website}`}>Website</a>
+          </li>
+      );
+    })}
+      </ul>
+      </div>
+      </div>
+    );
+  }
+}
+}
+
+
+export default App;
